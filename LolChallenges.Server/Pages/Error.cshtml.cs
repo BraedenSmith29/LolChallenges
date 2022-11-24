33becf8248b64438d10244af1e19598b42551865
Braeden Smith <braedensmith629@gmail.com>
2022-11-24 17:01:33 -0600

    Switched model from server to wasm with asp.net backend

--- LolChallenges.Server/Pages/Error.cshtml.cs
+++ LolChallenges.Server/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace LolChallenges.Web.Pages
+namespace LolChallenges.Server.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
